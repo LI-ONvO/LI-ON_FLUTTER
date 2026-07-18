0806f09d9ff98f59215a41da4051475856ba74c0
최승우 <choi0250655@gmail.com>
2026-07-19 00:37:16 +0900

    feat : 디자인 시스템 기본 요소 추가
      HelveticaNeue 폰트 패밀리 등록
      AppColors, AppTextStyle, AppSpacing    상수 정의
     pubspec.yaml에 폰트/에셋 설정 반영

---
+++ lib/core/constants/font.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:li_on/core/constants/color.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static const TextStyle baseTextStyle = TextStyle(
+    fontFamily: 'HelveticaNeue',
+    color: AppColors.text,
+  );
+
+  static TextStyle bold = baseTextStyle.copyWith(
+    fontWeight: FontWeight.bold,
+    fontSize: 24,
+  );
+
+  static TextStyle semiBold = baseTextStyle.copyWith(
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+  );
+
+  static TextStyle section = baseTextStyle.copyWith(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle card = baseTextStyle.copyWith(
+    fontWeight: FontWeight.w500,
+    fontSize: 15,
+  );
+
+  static TextStyle mainText = baseTextStyle.copyWith(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: AppColors.subText,
+  );
+
+  static TextStyle subText = baseTextStyle.copyWith(
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    color: AppColors.subText,
+  );
+}
