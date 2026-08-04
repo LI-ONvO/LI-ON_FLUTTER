6d9e9d57c85e45d7395be6c9d59c236b41a4be85
최승우 <choi0250655@gmail.com>
2026-08-04 23:06:20 +0900

    사용하지 않는 import 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:li_on/pages/auth/login/view/login_page.dart';
 import 'package:li_on/pages/onboarding/view/onboarding_page.dart';
 
 void main() {
